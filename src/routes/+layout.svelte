b22d38903d2f9801e9fae3078b851c8305f4a63c
Kiri <cultivatingghoul@gmail.com>
2026-05-28 21:22:23 +0200

    added site name fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <svelte:head>
-	<title>Grimoji |  Gothic Emoji Alchemy Puzzle Game</title>
+	<title>Grimoji | Gothic Emoji Alchemy Puzzle Game</title>
 	<meta
 		name="description"
 		content="Merge magical elements on a dark runic grid. Discover 800+ unique emoji recipes in Grimoji, the ultimate gothic alchemy puzzle game combining 2048 and match-3 mechanics."
@@ -18,26 +18,38 @@
 	<link rel="canonical" href="https://grimoji.io" />
 	<link rel="icon" href={favicon} />
 
+	<meta property="og:site_name" content="Grimoji" />
+	<meta property="og:title" content="Grimoji | Official Gothic Alchemy Puzzle Game" />
+	<meta property="og:url" content="https://grimoji.io/" />
+
 	{@html `
     <script type="application/ld+json">
-    {
-      "@context": "https://schema.org",
-      "@type": "VideoGame",
-      "name": "Grimoji",
-      "url": "https://grimoji.io",
-      "description": "A gothic alchemy puzzle game blending grid-sliding mechanics of 2048 with match-3 chain reactions.",
-      "genre": ["Puzzle Game", "Logic Game"],
-      "author": {
-        "@type": "Person",
-        "name": "Christin Nyakanyanga"
+    [
+      {
+        "@context": "https://schema.org",
+        "@type": "WebSite",
+        "name": "Grimoji",
+        "url": "https://grimoji.io/"
       },
-      "publisher": {
-        "@type": "Organization",
-        "name": "Ghetto Coders"
-      },
-      "applicationCategory": "Game",
+      {
+        "@context": "https://schema.org",
+        "@type": "VideoGame",
+        "name": "Grimoji",
+        "url": "https://grimoji.io",
+        "description": "A gothic alchemy puzzle game blending grid-sliding mechanics of 2048 with match-3 chain reactions.",
+        "genre": ["Puzzle Game", "Logic Game"],
+        "author": {
+          "@type": "Person",
+          "name": "Christin Nyakanyanga"
+        },
+        "publisher": {
+          "@type": "Organization",
+          "name": "Ghetto Coders"
+        },
+        "applicationCategory": "Game",
       "operatingSystem": "Android, IOS, Windows, Linux, MacOS"
-    }
+      }
+    ]
     <\/script>
   `}
 </svelte:head>
